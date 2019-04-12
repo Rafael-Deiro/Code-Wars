244d450ee9c2ea75eeb24af0be869532a26b2f0c
Rafael Deiro <rafaeldeirolopes@gmail.com>
2019-04-12 10:58:33 -0300

    6 kyu Is Prime Kata added Added PHP/6kyu, PHP/6kyu/README.md, PHP/6kyu/is_prime.php as well as it`s log file. Indexed 6 kyu Is Prime in README.md Fixed log path in PHP/default_model.php

--- PHP/6kyu/README.md
+++ PHP/6kyu/README.md
@@ -4,7 +4,9 @@ Common, bp_, min_ and min_bp_ Kata from 6 kyu rank for PHP.
 ## Available Kata
 + **Common**
     - [*Alphabet Indexer*](alphabet_indexer.php "alphabet_indexer.php")
+    - [*Is Prime*](is_prime.php "is_prime.php")
 
 ## Log files
 + [**Logs**](logs/ "logs")
     - [*Alphabet Indexer Logs*](logs/alphabet_indexer.log.txt "alphabet_indexer.log.txt")
+    - [*Is Prime Logs*](logs/is_prime.log.txt "is_prime.log.txt")
--- PHP/6kyu/is_prime.php
+++ PHP/6kyu/is_prime.php
@@ -4,46 +4,73 @@
 */
 
 /*
-Kata URI: URI
-Kata Author: AUTHOR_NAME
+Kata URI: https://www.codewars.com/kata/5262119038c0985a5b00029f
+Kata Author: arithmetric
 
 Description:
-  Kata task description
+  Define a function isPrime()/is_prime() that receives an integer and returns true if it`s a prime number or false if it isn`t prime.
+  A prime number is a natural number greater than 1 that has no divisors other than 1 and itself.
+  You may receive integers lower than 0.
 
 Example:
-  Kata task example
+  isPrime(5): true
+
+  is_prime(10002): false
 
 Tags:
-  Kata tags
+  Algorithms, Mathematics, Numbers
 */
 
 /* ---------------------------- My Solution ---------------------------- */
 
 /**
- * Kata functions class
+ * Arithmetic functions class
 */
-class kataClass{
+class arithmetic{
 
   /**
-   * Main kata function
+   * Evaluates if $n integer is either a prime number or not
+   * 
+   * @param int $n Integer value to be evaluated
+   * @return bool True if $n is prime. Otherwise, false is returned
   */
-  public function kataFunction(){
+  public function is_prime($n){
+    if ($n < 2){
+      return false;
+    }
+    
+    for ($i = 2; $i <= $n; $i++){
+      if ($n/$i < $i){
+        return true;
+      }
+      elseif ($n % $i == 0){
+        return false;
+      }
+      elseif ($n == $i){
+        return true;
+      }
+    }
+  }
 
+  public function isPrime($n){
+    return $this->is_prime($n);
   }
 }
 
 /**
- * Tests kata class
+ * Tests arithmetic class
 */
-class testKataClass extends kataClass{
+class testArithmetic extends arithmetic{
 
   /**
-   * Main test function
+   * is_prime()/isPrime() test function
   */
-  public function testFunction(){
-    $this->checkValidity($this->kataFunction, 'EXPECTED', true);
+  public function testIsPrimeShouldReturnBool(){
+    $this->checkValidity($this->isPrime(5), true);
+    $this->checkValidity($this->is_prime(-7), false);
+    $this->checkValidity($this->isPrime(rand(-100000, 1)), false);
 
-    echo "The assertion logs can be found at /PHP/KYU/logs/" . "LOG_FILE_NAME" . "\n";
+    echo "The assertion logs can be found at /PHP/6kyu/logs/is_prime.log.txt\n";
   }
 
   /**
@@ -52,9 +79,10 @@ class testKataClass extends kataClass{
    * @param mixed $actual Actual value given to the assertion/ Usually the output of a function
    * @param mixed $expected Expected value given to the assertion. Has to be known prior to the test, otherwise, the assertion may be invalid even though the function works correctly.
    * @param bool $sameType Tells the function to either check the assertion values types or not (=== operator on/of). True checks, false doesn`t.
+   * @param bool $log Tells the function to either log the results to a file or not. True logs, false doesn`t.
    * @return bool True if the assertion is valid. False if the assertion is invalid.
   */
-  public function checkValidity($actual, $expected, $sameType = false){
+  public function checkValidity($actual, $expected = null, $sameType = true){
     $actualStr = "";
     $expectedStr = "";
 
@@ -63,6 +91,9 @@ class testKataClass extends kataClass{
     }
     elseif ($actual === false){
       $actualStr = "false";
+    }
+    elseif ($actual === null){
+      $actualStr = "nothing";
     }
     else {
       $actualStr = strval($actual);
@@ -74,23 +105,32 @@ class testKataClass extends kataClass{
     elseif ($expected === false){
       $expectedStr = "false";
     }
+    elseif ($expected === null){
+      $expectedStr = "nothing";
+    }
     else {
       $expectedStr = strval($expected);
     }
 
-    if ($sameType === true){
+    if ($sameType === true && $expected !== null){
       if ($actual === $expected){
         $log = [[$actualStr, $expectedStr], "PASS: The actual match with the expected in value and type"];
         $this->logTest($log[0], $log[1]);
         return true;
       }
     }
-    else {
+    elseif ($expected !== null){
       if ($actual == $expected){
         $log = [[$actualStr, $expectedStr], "PASS: The actual match with the expected in value"];
         $this->logTest($log[0], $log[1]);
         return true;
       }
+    }
+
+    if ($expected === null){
+      $log = [[$actualStr, $expectedStr], "UNKNOWN: No expected value"];
+      $this->logTest($log[0], $log[1]);
+      return true;
     }
 
     $log = [[$actualStr, $expectedStr], "FAIL: The actual doesn`t match with the expected either in value or type."];
@@ -105,7 +145,7 @@ class testKataClass extends kataClass{
    * @param string $log Log message to be recorded in the file.
   */
   public function logTest(array $values, string $log){
-    $logFile = fopen('logs/' . 'LOG_FILE_NAME', 'a+');
+    $logFile = fopen('logs/is_prime.log.txt', 'a+');
 
     $logString = date('Y/m/d @ H:i:s') . " | $log \nExpected: {$values[1]} | Result: {$values[0]}\n\n";
 
@@ -114,4 +154,6 @@ class testKataClass extends kataClass{
   }
 }
 
-// Test function call
+$testArithmetic = new testArithmetic();
+$testArithmetic->testIsPrimeShouldReturnBool();
+
